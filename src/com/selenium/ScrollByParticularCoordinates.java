e25092e48120612584afeffd1fddca331959d27b
Gowthami <gowthamipulicherla83@gmailcom>
2021-09-30 13:34:33 +0530

    Commit message

---
+++ src/com/selenium/ScrollByParticularCoordinates.java
@@ -0,0 +1,14 @@
+package com.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class ScrollByParticularCoordinates {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.edge.driver","C:\\Users\\gowthami.pulicherla\\Music\\Softwares\\msedgedriver.exe");
+		WebDriver driver=new EdgeDriver();
+	}
+
+}
---
+++ src/com/selenium/ScrollTillParticularElement.java
@@ -0,0 +1,16 @@
+package com.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class ScrollTillParticularElement {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.edge.driver","C:\\Users\\gowthami.pulicherla\\Music\\Softwares\\msedgedriver.exe");
+		WebDriver driver=new EdgeDriver();
+		
+
+	}
+
+}
